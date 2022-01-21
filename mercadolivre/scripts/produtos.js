190bcce05e7d890456f7b2c74b413cf146768e52
Diego Rodrigues <diego.rodrigues@enjoei.com.br>
2022-01-20 23:03:00 -0300

    requisicao para nossa API (BETA)

---
+++ mercadolivre/scripts/produtos.js
@@ -0,0 +1,20 @@
+function buscarProdutos() {
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function () {
+		if (this.readyState == 4 && this.status == 200) { // HTTP STATUS CODE					
+			var produtos = JSON.parse(this.responseText);
+			mostrarProdutos(produtos);
+		}
+	};
+	
+	//JEITO 2 (concatenar - interpolação)
+	let endpoint = `http://localhost:3000/produtos`;
+
+	xhttp.open("GET", endpoint, true); // PREPARADANDO A REQUEST
+	xhttp.send(); // REQUEST (REQUISIÇÃO) 			
+}
+
+function mostrarProdutos(produtos){
+	let produtosView = document.getElementById("produtosView");
+	produtosView.innerText = produtos;
+}
